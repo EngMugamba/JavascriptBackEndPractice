0b03d6fe7154569fe28123d2c1eff34fba198c12
EngMugamba <mugambakelvin0@gmail.com>
2023-03-03 13:08:43 +0300

    added all files

--- myApp.js
+++ myApp.js
@@ -4,10 +4,16 @@ let app = express();
 
 
 
+app.get("/", function(req, res) {
+    res.sendFile(__dirname + "/views/index.html");
+  });
 
 
+// Normal usage
 
 
+// Assets at the /public route
+app.use("/public", express.static(__dirname + "/public"));
 
 
 
